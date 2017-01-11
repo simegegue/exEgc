642ceaf2a7ab6d00f47796d667ba0e576e54cff1
simegegue <simon.egea48@gmail.com>
2017-01-11 16:56:14 +0100

    Maven

--- src/main/java/es/egc/App1.java
+++ src/main/java/es/egc/App1.java
@@ -1,5 +1,7 @@
 package es.egc;
-import org.apache.log4j.*; 
+
+
+
 
 /**
  * DIVISION
@@ -7,7 +9,7 @@ import org.apache.log4j.*;
  */
 public class App1 
 {
-	static Logger log=Logger.getLogger(App1.class); 
+	
 	
 	public static int divide(int a, int b){
 		int result= a/b;
--- src/main/java/es/egc/App2.java
+++ src/main/java/es/egc/App2.java
@@ -1,13 +1,13 @@
 package es.egc;
-import org.apache.log4j.*; 
+
 
 /**
  * DIVISION
  *
  */
-public class App1 
+public class App2 
 {
-	static Logger log=Logger.getLogger(App1.class); 
+
 	
 	public static int divide(int a, int b){
 		int result= a/b;
@@ -17,13 +17,13 @@ public class App1
 	
 	public static int multiplica(int a, int b){
 		int result= a*b;
-		log.info("Returning "+a+"*"+b+"="+result);
+		
 		return result;
 	}
 	
 	public static int suma(int a, int b){
 		int result=a-b;
-		log.info("Returning "+a+"*"+b+"="+result);
+		
 		return result;
 	}
 }
