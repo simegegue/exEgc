eb0d0925d60cf6d22710037238087ff8fb1bd0f8
simegegue <simon.egea48@gmail.com>
2017-01-11 16:48:29 +0100

    P

--- src/main/java/es/egc/App2.java
+++ src/main/java/es/egc/App2.java
@@ -15,5 +15,15 @@ public class App1
 		return result;
 	}
 	
+	public static int multiplica(int a, int b){
+		int result= a*b;
+		log.info("Returning "+a+"*"+b+"="+result);
+		return result;
+	}
 	
+	public static int suma(int a, int b){
+		int result=a-b;
+		log.info("Returning "+a+"*"+b+"="+result);
+		return result;
+	}
 }
